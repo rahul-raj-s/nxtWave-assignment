05d5cc458141d46769ff027f430673ff1a061118
rahul.raj <rahul.raj@quantuminventions.com>
2022-12-25 19:28:00 +0530

    feat: style the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,17 @@
-export const Card = ({ title, iconUrl }) => {
+export const Card = ({ title, iconUrl, subtitle, link, description }) => {
   return (
     <div className="card">
-      <div>
-        <img src={iconUrl} height="20px" width="20px"/>
-        <h1>{title}</h1>
+      <div className="card_header">
+        <img src={iconUrl} className="card_icon" />
+        <div>
+          <h1 className="cart_title">{title}</h1>
+          <small>{subtitle}</small>
+        </div>
       </div>
+      <a href={link} className="card_link">
+        {link}
+      </a>
+      <p className="card_description">{description}</p>
     </div>
   );
 };
