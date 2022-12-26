a8ccb24683f0c8c351eea1a38cc72d706a0e59ec
rahul.raj <rahul.raj@quantuminventions.com>
2022-12-26 11:49:29 +0530

    feat: setup the toastify

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,12 @@
-export const Button = ({ variant = "primary", className = "", children }) => {
+export const Button = ({
+  variant = "primary",
+  className = "",
+  disabled = false,
+  children,
+}) => {
   return (
-    <button className={`btn btn_${variant} ${className}`}>{children}</button>
+    <button className={`btn btn_${variant} ${className}`} disabled={disabled}>
+      {children}
+    </button>
   );
 };
--- src/services/serviceHelper.js
+++ src/services/serviceHelper.js
@@ -1,34 +1,7 @@
-const pathWithQuery = (path, queryParams) => {
-  let finalPath = `${path}`;
-  if (queryParams) {
-    finalPath = `${finalPath}?`;
-    for (const property in queryParams) {
-      finalPath = `${finalPath}${property}=${queryParams[property]}&`;
-    }
-  }
-  return finalPath;
-};
-
-
-export const createMutation = (builder, path) => {
-  return builder.mutation({
-    query: (queryParams) => ({
-      url: path,
-      method: "post",
-      body: queryParams.formData,
-    }),
-    invalidatesTags: (result, error, id) => {
-      if (!error) {
-        return [path];
-      }
-    },
-  });
-};
-
 export const getCollectionQuery = (builder, path) => {
   return builder.query({
-    query: (queryParams) => ({
-      url: pathWithQuery(path, queryParams),
+    query: () => ({
+      url: path,
       method: "get",
     }),
     keepUnusedDataFor: 2,
