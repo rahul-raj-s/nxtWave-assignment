6d83c9021641740b24fd2b23bb38615bdfc362c7
rahulrajqi <rahul.raj@quantuminventions.com>
2022-12-26 14:49:44 +0530

    fix: react router setup using hashnode

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.StrictMode>
-      <HashRouter>
+      <HashRouter >
         <App />
       </HashRouter>
     </React.StrictMode>
